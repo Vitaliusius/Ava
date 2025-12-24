24bca3465c12845e022ec1f02b22444c21b916ff
Vitaliy <vitalianobro@gmail.com>
2025-12-24 19:08:13 +0300

    main1.py

--- main1.py
+++ main1.py
@@ -14,7 +14,7 @@ cropred2 = red.crop(coordinates)
 red_end = Image.blend(cropred1, cropred2, 0.5)
 
 
-coordinates = (, 0, blue.width-30, blue.height)
+coordinates = (0, 0, blue.width-30, blue.height)
 cropblue1 = blue.crop(coordinates)
 
 coordinates = (15, 0, blue.width-15, blue.height)
@@ -30,6 +30,7 @@ full_image = Image.merge("RGB",(red_end,green_end,blue_end))
 
 full_image.thumbnail((80,80))
 full_image.save("ava.jpg")
+
 
 
 
