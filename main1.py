28adbda95683894b29b36d6b0f9aaf48ef6222c3
Vitaliy <vitalianobro@gmail.com>
2025-12-24 13:59:20 +0300

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,52 @@
+from PIL import Image
+
+ava = Image.open("C:/python_scripts/ava/monro.jpg")
+red, blue, green = ava.split()
+
+
+
+coordinates = (30, 0, red.width, red.height)
+cropred1 = red.crop(coordinates)
+
+coordinates = (0, 0, red.width-30, red.height)
+cropred2 = red.crop(coordinates)
+
+red_end = Image.blend(cropred1, cropred2, 0.5)
+
+
+coordinates = (0, 0, blue.width-30, blue.height)
+cropblue1 = blue.crop(coordinates)
+
+coordinates = (30, 0, blue.width, blue.height)
+cropblue2 = blue.crop(coordinates)
+
+blue_end = Image.blend(cropblue1, cropblue2, 0.5)
+
+
+coordinates = (15, 0, green.width-15, green.height)
+green_end = green.crop(coordinates)
+
+full_image = Image.merge("RGB",(red_end,green_end,blue_end))
+full_image.save("C:/python_scripts/ava/full_image.jpg")
+
+full_image.thumbnail((80,80))
+full_image.save("C:/python_scripts/ava/ava.jpg")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
